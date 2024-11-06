809f6db2d4a95cd7c8c72b83c06c612bf739ef2a
Herrerasaurus <146883762+Herrerasaurus@users.noreply.github.com>
2024-11-06 13:22:35 -0800

    Updating app element

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import "./style.css";
 
-const app = document.getElementById("app");
+const app: HTMLDivElement = document.querySelector("#app")!;
 const button = document.createElement("button");
 
 button.addEventListener("click", () => {
     const message = document.createElement("p");
     message.textContent = "you clicked the button!";
-    app?.append(message);
+    app.append(message);
 });
-app?.append(button);
+app.append(button);
