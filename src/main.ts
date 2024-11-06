bd3afc2970e312bcf7babd9110b180d524be5c6c
Herrerasaurus <146883762+Herrerasaurus@users.noreply.github.com>
2024-11-06 13:25:21 -0800

    updating alert message

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 const button = document.createElement("button");
+const message = document.createElement("p");
 
 button.addEventListener("click", () => {
-    const message = document.createElement("p");
-    message.textContent = "you clicked the button!";
+    message.innerHTML = "you clicked the button!";
     app.append(message);
 });
 app.append(button);
