812d79684a7ee4ad250c578f699b947582e8ec26
Herrerasaurus <146883762+Herrerasaurus@users.noreply.github.com>
2024-11-15 14:16:01 -0800

    implementing board file to main

--- src/main.ts
+++ src/main.ts
@@ -21,17 +21,16 @@ const CACHE_SPAWN_PROBABILITY = 0.1;
 const knownCells: Map<string, Cache> = new Map();
 
 //generate board
-const board = new Board(10, 10);
+const board = new Board(1, NEIGHBORHOOD_SIZE);
 
 // center it on the oaks classroom location
-const OAKES_CLASSROOM_CELL: Cell = { i: 369894, j: -1220627 };
-const cellBounds = board.getCellBounds(OAKES_CLASSROOM_CELL);
-const centerLat = (cellBounds.getNorth() + cellBounds.getSouth()) / 2;
-const centerLng = (cellBounds.getEast() + cellBounds.getWest()) / 2;
+const OAKES_LAT = 369894 * TILE_DEGREES
+const OAKES_LNG = -1220627 * TILE_DEGREES;
+const OAKES_CLASSROOM = new leaflet.LatLng(OAKES_LAT, OAKES_LNG);
 
 // Create the map (element with id "map" is defined in index.html)
 const map = leaflet.map(document.getElementById("map")!, {
-  center: [centerLat, centerLng],
+  center: OAKES_CLASSROOM,
   zoom: GAMEPLAY_ZOOM_LEVEL,
   minZoom: GAMEPLAY_ZOOM_LEVEL,
   maxZoom: GAMEPLAY_ZOOM_LEVEL,
@@ -49,81 +48,8 @@ leaflet
   .addTo(map);
 
 // Add a marker to represent the player
-const playerMarker = leaflet.marker([centerLat, centerLng]);
+const playerMarker = leaflet.marker(OAKES_CLASSROOM);
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
+console.log(OAKES_LAT);
 
-// Display the player's points
-let playerPoints = 0;
-const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!; // element `statusPanel` is defined in index.html
-statusPanel.innerHTML = "No points yet...";
-
-// Add caches to the map by cell numbers
-interface Cell {
-  readonly i: number;
-  readonly j: number;
-}
-
-interface Cache {
-  coins: number;
-  cell: Cell;
-}
-
-function spawnCache(c: Cache) {
-  // Convert cell numbers into lat/lng bounds
-  const bounds = leaflet.latLngBounds([
-    [
-      centerLat + c.cell.i * TILE_DEGREES,
-      centerLng + c.cell.j * TILE_DEGREES,
-    ],
-    [
-      centerLat + (c.cell.i + 1) * TILE_DEGREES,
-      centerLng + (c.cell.j + 1) * TILE_DEGREES,
-    ],
-  ]);
-
-  // Add a rectangle to the map to represent the cache
-  const rect = leaflet.rectangle(bounds);
-  rect.addTo(map);
-
-  // Handle interactions with the cache
-  rect.bindPopup(() => {
-    // Each cache has a random point value, mutable by the player
-    c.coins = Math.floor(
-      luck([c.cell.i, c.cell.j, "initialValue"].toString()) * 100,
-    );
-
-    // The popup offers a description and button
-    const popupDiv = document.createElement("div");
-    popupDiv.innerHTML = `
-                <div>There is a cache here at "${c.cell.i},${c.cell.j}". It has value <span id="value">${c.coins}</span>.</div>
-                <button id="collect">collect</button>`;
-
-    // Clicking the button decrements the cache's value and increments the player's points
-    popupDiv
-      .querySelector<HTMLButtonElement>("#collect")!
-      .addEventListener("click", () => {
-        CollectCoin(c);
-        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML = c.coins
-          .toString();
-      });
-
-    return popupDiv;
-  });
-}
-
-function CollectCoin(cache: Cache) {
-  playerPoints++;
-  cache.coins--;
-  statusPanel.innerHTML = `${playerPoints} points accumulated`;
-}
-
-for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
-      const cache = { coins: 0, cell: { i, j } };
-      knownCells.set([i, j].toString(), cache);
-      spawnCache(cache);
-    }
-  }
-}
