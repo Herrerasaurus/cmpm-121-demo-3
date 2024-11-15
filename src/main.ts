4a8391cd342b54c899da6cc6ff1ed54781191243
Herrerasaurus <146883762+Herrerasaurus@users.noreply.github.com>
2024-11-15 15:21:31 -0800

    implementing coins with serial numbers

--- src/main.ts
+++ src/main.ts
@@ -21,10 +21,10 @@ const CACHE_SPAWN_PROBABILITY = 0.1;
 const knownCells: Map<string, Cache> = new Map();
 
 //generate board
-const board = new Board(1, NEIGHBORHOOD_SIZE);
+const board = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
 
 // center it on the oaks classroom location
-const OAKES_LAT = 369894 * TILE_DEGREES
+const OAKES_LAT = 369894 * TILE_DEGREES;
 const OAKES_LNG = -1220627 * TILE_DEGREES;
 const OAKES_CLASSROOM = new leaflet.LatLng(OAKES_LAT, OAKES_LNG);
 
@@ -51,5 +51,89 @@ leaflet
 const playerMarker = leaflet.marker(OAKES_CLASSROOM);
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
-console.log(OAKES_LAT);
+console.log(OAKES_CLASSROOM);
+const playerCoins: Coin[] = [];
 
+// Display the player's points
+let playerPoints = 0;
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!; // element `statusPanel` is defined in index.html
+statusPanel.innerHTML = "No points yet...";
+
+// Convert the classroom location to a grid cell
+const classroomCell = board.getCellForPoint(OAKES_CLASSROOM);
+const bounds = board.getCellBounds(classroomCell);
+
+// Add a rectangle to the map to represent the cache
+const rect = leaflet.rectangle(bounds);
+rect.addTo(map);
+
+// get cells near oaks
+const neighborhood = board.getCellsNearPoint(OAKES_CLASSROOM);
+
+// for each cell in the neighborhood, spawn a cache
+for (const cell of neighborhood) {
+  //randomly spawn cache
+  if (luck([cell.i, cell.j].toString()) < CACHE_SPAWN_PROBABILITY) {
+    spawnCache(cell.i, cell.j);
+  }
+}
+
+// Spawn a cache at a given cell
+function spawnCache(i: number, j: number) {
+  const cell = board.getCellForPoint(
+    new leaflet.LatLng(i * TILE_DEGREES, j * TILE_DEGREES),
+  );
+  const bounds = board.getCellBounds(cell);
+  // Add a rectangle to the map to represent the cache
+  const rect = leaflet.rectangle(bounds);
+  rect.addTo(map);
+
+  // Handle interactions with the cache
+  rect.bindPopup(() => {
+    // Each cache has a random point value, mutable by the player
+    const coinArray: Coin[] = [];
+    const pointValue = Math.floor(
+      luck([i, j, "initialValue"].toString()) * 100,
+    );
+    for (let i = 0; i < pointValue; i++) {
+      coinArray.push(generateCoins(cell, i));
+    }
+
+    // The popup offers a description and button
+    const popupDiv = document.createElement("div");
+    popupDiv.innerHTML = `
+                <div>There is a cache here at "${cell.i}, ${cell.j}". It has value <span id="value">${pointValue}</span>.</div>
+                <button id="collect">collect</button>`;
+
+    // Clicking the button decrements the cache's value and increments the player's points
+    popupDiv
+      .querySelector<HTMLButtonElement>("#collect")!
+      .addEventListener("click", () => {
+        CollectCoin(coinArray);
+        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+          (pointValue - 1)
+            .toString();
+      });
+
+    return popupDiv;
+  });
+}
+
+function CollectCoin(coinArray: Coin[]) {
+  playerPoints++;
+  playerCoins.push(coinArray.pop()!);
+  statusPanel.innerHTML = `${playerPoints} points accumulated`;
+}
+
+// adding serial number identification to each coin
+interface Coin {
+  location: { i: number; j: number };
+  serial: number;
+}
+
+function generateCoins(cell: Cell, serialNum: number): Coin {
+  return {
+    location: { i: cell.i, j: cell.j },
+    serial: serialNum,
+  };
+}
